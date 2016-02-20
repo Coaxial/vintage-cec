fac871a65c96010d2eaf94895f325533bb51a106
Coaxial <py@poujade.org>
2016-02-19 22:50:38 -0500

    wip: Join wifi network

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "Arduino.h"
 #include <IRremoteESP8266.h>
 #include <IRremoteInt.h>
+#include <ESP8266WiFi.h>
+
+#include "secrets.h"
 
 const int VOL_UP = 0x490;
 const int VOL_DOWN = 0xc90;
@@ -12,26 +15,53 @@ IRrecv irrecv(RECV_PIN);
 
 decode_results results;
 
-void setup() {
-  Serial.begin(115200);
+void setupDebugConsole() {
+  Serial1.begin(115200);
+  Serial1.setDebugOutput(true);
+  Serial1.println("");
+  Serial1.println("Debug console ready");
+}
+
+void connectWiFi() {
+  Serial1.print("Connecting to ");
+  Serial1.println(wlan_ssid);
+
+  WiFi.begin(wlan_ssid, wlan_password);
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial1.print('.');
+  }
+
+  Serial1.println("");
+  Serial1.print("Wifi connected with IP: ");
+  Serial1.println(WiFi.localIP());
+}
+
+void listenForIR() {
   irrecv.enableIRIn();
-  Serial.println("Listening for Sony beams");
+}
+
+void setup() {
+  setupDebugConsole();
+  connectWiFi();
+  listenForIR();
 }
 
 void loop() {
   irrecv.decode(&results);
   if (irrecv.decode(&results)) {
     if(results.decode_type == SONY) {
-      Serial.println(results.value, HEX);
+      Serial1.println(results.value, HEX);
       switch(results.value) {
         case VOL_UP:
-          Serial.println("VOL UP");
+          Serial1.println("VOL UP");
           break;
         case ARROW_UP:
-          Serial.println("ARROW UP");
+          Serial1.println("ARROW UP");
           break;
         case VOL_DOWN:
-          Serial.println("VOL DOWN");
+          Serial1.println("VOL DOWN");
           break;
       }
     }
